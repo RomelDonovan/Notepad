0c54662340c9eb009dc49fecb115772438c9a787
Romel Donovan <romeldonovan@live.ca>
2022-10-21 21:50:41 -0400

    First Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+# Simple To-Do list
+
+# User Info
+def username():
+    pass
+
+# User time/date
+def date_time():
+    pass
+
+# Add Edit Delete
+def task_control():
+    pass
+
+# Display to user
+def display_task():
+    pass
+
+def main():
+    username()
+    date_time()
+    task_control()
+    display_task()
+
+if __name__ == '__main__':
+    main()
