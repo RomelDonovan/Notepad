bb934df859674c86d87b620b68499f1a947563bb
Romel Donovan <romeldonovan@live.ca>
2022-10-23 14:50:56 -0400

    First Commit

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 # Simple To-Do list
 
-# User Info
-def username():
+# Users list of tasks
+user_list = []
+
+#1 Add tasks
+def add_task(task):
+    while True:
+        user_list.append(task)
+        # print(user_list)
+        ans = input("\nWant to add more?\n")
+        if ans == "yes":
+            task = input("\nWhat to do?:\n")
+        new_list = '\n'.join(user_list)
+        if ans == 'no':
+            ans = False
+            break
+    print(new_list)
+
+
+# Edit tasks
+def edit_task():
     pass
 
-# User time/date
-def date_time():
+# Delete tasks
+def delete_task():
     pass
 
-# Add Edit Delete
-def task_control():
-    pass
-
-# Display to user
-def display_task():
+# Users view of tasks
+def display_tasks():
     pass
 
 def main():
-    username()
-    date_time()
-    task_control()
-    display_task()
+    user_task = input("\nWhat to do?:\n")
+    add_task(user_task)
+    edit_task()
+    delete_task()
+    display_tasks()
+
 
 if __name__ == '__main__':
     main()
