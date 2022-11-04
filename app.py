2a45b26e4abf3be968bea7fe971b604f81d01fd0
romeldonovan <romeldonovan@live.ca>
2022-11-04 19:50:51 -0400

    app is functional

--- app.py
+++ app.py
@@ -18,21 +18,38 @@ def add_note(connection):
 
 def edit_note(connection):
     cursor = connection.cursor()
-    edit_sql = "UPDATE notes SET"
-    cursor.execute(edit_sql)
-    final_results = cursor.fetchone()
-    connection.commit()
+    try:
+        id_num = int(input("Which number would you like to edit?: "))
+        new_note = input("New Note: ")
+        query = "UPDATE notes SET note = %s WHERE id = %s"
+        cursor.execute(query, (new_note,id_num))
+        connection.commit()
+    except:
+        print("Sorry invalid number.")
 
 def delete_note(connection):
     cursor = connection.cursor()
-    delete_query = "DELETE FROM notes WHERE id = "
-    cursor.execute(delete_query)
-    print("deleted")
-    connection.commit()
+    try:
+        delete_id = input("Please enter the number you want to delete: ")
+        query = "DELETE FROM notes WHERE id = %s" 
+        cursor.execute(query, (delete_id,))
+        print("Deleted!")
+        connection.commit()
+    except:
+        print("Sorry invalid number.")
 
-def display_notes(cursor):
-    cursor.execute()
+def display_notes(connection):
+    cursor = connection.cursor()
+    num_list = 0
+    query = "SELECT note FROM notes"
+    cursor.execute(query)
     all_notes = cursor.fetchall()
+    for note in all_notes:
+        num_list += 1
+        # print(num_list)
+        print(f"{str(num_list)}. {''.join(note)}")
+        # view_list = num_list + note
+        # print(view_list)
 
 def main():
     connection = db_connect()
